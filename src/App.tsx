bb134a56e0a7b8802b813cd0f948e2b78091ab37
FatlumHaliti <fatlum.haliti@mithings.se>
2024-06-23 00:37:05 +0200

    secind

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code>
+          Edit <code>src/App.tsx</code> Logo
         </p>
         <a
           className="App-link"
