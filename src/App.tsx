247f380afbcc844c0cd5f04f6c4f556d70898b92
FatlumHaliti <fatlum.haliti@mithings.se>
2024-10-23 14:43:34 +0200

    added three/fiber-drei lib and made a sqaure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <div className="App">
-      HELLO
+      <Canvas gl={{antialias: false}} dpr={[1,1.5]}>
+        <mesh>
+          <boxGeometry/>
+        </mesh>
+      </Canvas>
     </div>
   );
 }
