b06a1bda1a3f02848bee45dabe7fa2f34d6aa21d
Fatlum Haliti <fatlum-haliti@outlook.com>
2024-12-09 15:56:04 +0100

    added 2 projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,18 @@ const Projects = () => {
     return (
         <div id='projects' className='mt-[100vh]' >
             Projects
+            <div className='flex flex-wrap justify-evenly'>
+                <div className='w-[600px] h-fit bg-white rounded-lg'>  
+                    <a className='w-full' href='https://vuejs-projects-2ad81.web.app/' target='_blank'>
+                        <img src={'./assets/kantur-home-min.png'} className='w-full' />
+                    </a>                  
+                </div>
+                <div className='w-[600px] h-fit bg-white rounded-lg'>  
+                    <a className='w-full' href='https://vuejs-projects-2ad81.web.app/' target='_blank'>
+                        <img src={'./assets/vue-live-chat-min.png'} className='w-full' />
+                    </a>                  
+                </div>
+            </div>
         </div>
     )
 }
