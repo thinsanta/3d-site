ca0d2b159885f3338b44d3ed6f3edefc5905224b
Fatlum Haliti <fatlum-haliti@outlook.com>
2024-12-09 21:29:37 +0100

    on hover effect expands text

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,16 +4,30 @@ const Projects = () => {
     return (
         <div id='projects' className='mt-[100vh]' >
             Projects
-            <div className='flex flex-wrap justify-evenly'>
-                <div className='w-[600px] h-fit bg-white rounded-lg'>  
-                    <a className='w-full' href='https://vuejs-projects-2ad81.web.app/' target='_blank'>
+            <div className='flex flex-wrap justify-evenly gap-10 '>
+                <div className='container w-[600px] h-fit rounded-lg'>  
+                    <a className='w-full' href='https://kantur.se/' target='_blank'>
                         <img src={'./assets/kantur-home-min.png'} className='w-full' />
-                    </a>                  
+                    </a>
+                    <span className='role text-4xl'>
+                        Lead Web Developer
+                    </span>
+                    <div className='roleDesc hidden'>
+                    Took a lead developing job to create a job-portal for customers to be
+                    able to apply for roles.    
+                    </div>             
                 </div>
-                <div className='w-[600px] h-fit bg-white rounded-lg'>  
+                <div className=' container w-[600px] h-fit rounded-lg'>  
                     <a className='w-full' href='https://vuejs-projects-2ad81.web.app/' target='_blank'>
                         <img src={'./assets/vue-live-chat-min.png'} className='w-full' />
-                    </a>                  
+                    </a>   
+                    <span className='role text-4xl'>
+                        Hobby Project
+                    </span>
+                    <div className='roleDesc hidden'>
+                    Took a lead developing job to create a job-portal for customers to be
+                    able to apply for roles.    
+                    </div>             
                 </div>
             </div>
         </div>
