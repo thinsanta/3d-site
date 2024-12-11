d70835e445648db1374f6a670dba8e5a7fcbd6dd
Fatlum Haliti <fatlum-haliti@outlook.com>
2024-12-11 15:38:39 +0100

    added project info and buttons

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,8 @@
 import { Clone, ContactShadows, Environment, Float, Html, PresentationControls, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React, { Suspense } from 'react'
+import GithubLogo from '../assets/github-mark-white.svg'
+import Site from '../assets/site.svg'
 
 const Projects = () => {
 
@@ -21,6 +23,36 @@ const Projects = () => {
                                 <iframe src='https://vuejs-projects-2ad81.web.app/' />
                             </Html>
                         </primitive>
+                        <Html position-y={1}>
+                            <div className='w-[550px] text-2xl text-gray-400'>
+                             This was my first hobby project that I worked on after finishing school. It's a live chat where you can signup and chat with
+                             other users! I used Vuejs in the front and is connected to a firebase in the back that works as a database and websocket for 
+                             updating the live chat and has a CI/CD pipeline.
+
+                             <div className='flex flex-wrap items-center mt-5 justify-start gap-2 text-white'>
+                                <div className=' text-base bg-emerald-500 px-3 py-1 items-center rounded-md'>
+                                    Vuejs
+                                </div>
+                                <div className=' text-base bg-red-500 px-3 py-1 items-center rounded-md'>
+                                    Firebase
+                                </div>
+                                <div className=' text-base bg-orange-500 px-3 py-1 items-center rounded-md'>
+                                    Websocket
+                                </div>
+                                <div className=' text-base bg-blue-500 px-3 py-1 items-center rounded-md'>
+                                    Cloud(CI/CD)
+                                </div>
+                             </div>
+                             <a className='visitButton flex gap-3 mt-6 px-5 py-2 rounded-lg w-fit text-white items-center'>
+                                <div>
+                                    <img src={Site} height={"50px"} width={"50px"} />
+                                </div>
+                                <div className='text-base'>
+                                    Visit live
+                                </div>
+                             </a>
+                            </div>
+                        </Html>
                     </Float>
                     <Float rotationIntensity={0.4}>
                         <Clone object={macbook.scene} position-x={0.5} position-y={-1.3} scale={0.4} rotation-y={-0.3} rotation-z={0.05}>
